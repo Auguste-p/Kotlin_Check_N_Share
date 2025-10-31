e4b981ff3e22f88b0ddab2063e05f0bce592a743
Auguste-p <70906887+Auguste-p@users.noreply.github.com>
2025-10-31 09:49:30 +0100

    init

---
+++ app/src/main/java/com/example/checknshare/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.checknshare.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
